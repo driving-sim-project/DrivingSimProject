54e85c90558e6a490eb0cf30c4d4d958bae88b72
fireboat <fireboat999@hotmail.com>
2014-09-17 17:18:44 +0700

    Mirror Plane Prototype Commit
    Need to consider about performace

---
+++ Assets/Scripts/CSharpScripts/FillScreen.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (C) Stanislaw Adaszewski, 2013
+// http://algoholic.eu
+
+using UnityEngine;
+using System.Collections;
+
+public class FillScreen : MonoBehaviour {
+	
+	public Camera cam;
+	
+	public Transform portal1;
+	public Camera portal1Cam;
+
+	// Use this for initialization
+	void Start () {
+		// Camera.main.depthTextureMode = DepthTextureMode.Depth;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		//sky.position = cam.transform.position;
+		
+		Quaternion q = Quaternion.FromToRotation(-portal1.up, cam.transform.forward);
+        //portal1Cam.transform.position = portal1.position + (cam.transform.position - portal1.position);
+        //portal1Cam.transform.LookAt(portal1Cam.transform.position + q * portal1.up, portal1.transform.forward);
+        portal1Cam.nearClipPlane = (portal1Cam.transform.position - portal1.position).magnitude - 0.1f;
+	    
+		Vector3[] scrPoints = new Vector3[4];
+		scrPoints[0] = new Vector3(0, 0, 0.1f);
+		scrPoints[1] = new Vector3(1, 0, 0.1f);
+		scrPoints[2] = new Vector3(1, 1, 0.1f);
+		scrPoints[3] = new Vector3(0, 1, 0.1f);
+		
+		for (int i = 0; i < scrPoints.Length; i++) {
+			scrPoints[i] = transform.worldToLocalMatrix.MultiplyPoint(cam.ViewportToWorldPoint(scrPoints[i]));
+		}
+		
+		int[] tris = new int[6] {0, 1, 2, 2, 3, 0};
+		
+		MeshFilter mf = GetComponent<MeshFilter>();
+		mf.mesh.Clear();
+		mf.mesh.vertices = scrPoints;
+		mf.mesh.triangles = tris;
+		mf.mesh.RecalculateBounds();
+	}
+}
---
+++ Assets/Scripts/CSharpScripts/ScreenTexture.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+
+public class ScreenTexture : MonoBehaviour {
+    public int thumbProportion = 25;
+    public Color borderColor = Color.white;
+    public int borderWidth = 2;
+    public Camera mainCamera;
+    public Camera camera;
+    public GameObject mirrorPlane;
+    public float interval = 0.1f;
+    private Texture2D texture;
+    private Texture2D border; 
+    private int screenWidth;
+    private int screenHeight;
+    private bool  shoot = false;
+    private float time;
+
+
+    void  Start (){
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+        texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        time = Time.time;
+    }
+
+    private void OnPostRender()
+    {
+        if (Time.time > time + interval)
+        {
+            mainCamera.projectionMatrix = camera.projectionMatrix;
+            texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+            texture.Apply();
+            mirrorPlane.renderer.material.mainTexture = texture;
+            shoot = true;
+            Resources.UnloadUnusedAssets();
+            time = Time.time;
+        }
+    }
+}
